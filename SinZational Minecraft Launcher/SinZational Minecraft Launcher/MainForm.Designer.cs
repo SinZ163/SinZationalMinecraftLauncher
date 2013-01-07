9a28523444bb95744466fd5a5689a1c9ab6a36e5
Trent Monahan <sinz@d3s.co>
2013-01-07 13:03:02 +1100

    Add project

---
+++ SinZational Minecraft Launcher/SinZational Minecraft Launcher/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace SinZational_Minecraft_Launcher {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.userText = new System.Windows.Forms.TextBox();
+            this.passText = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.passLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // userText
+            // 
+            this.userText.Location = new System.Drawing.Point(549, 508);
+            this.userText.Name = "userText";
+            this.userText.Size = new System.Drawing.Size(100, 20);
+            this.userText.TabIndex = 0;
+            // 
+            // passText
+            // 
+            this.passText.Location = new System.Drawing.Point(549, 534);
+            this.passText.Name = "passText";
+            this.passText.Size = new System.Drawing.Size(100, 20);
+            this.passText.TabIndex = 1;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(666, 508);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(111, 46);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Launch!";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Location = new System.Drawing.Point(485, 508);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(58, 13);
+            this.userLabel.TabIndex = 3;
+            this.userLabel.Text = "Username:";
+            this.userLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // passLabel
+            // 
+            this.passLabel.AutoSize = true;
+            this.passLabel.Location = new System.Drawing.Point(487, 538);
+            this.passLabel.Name = "passLabel";
+            this.passLabel.Size = new System.Drawing.Size(56, 13);
+            this.passLabel.TabIndex = 4;
+            this.passLabel.Text = "Password:";
+            this.passLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 560);
+            this.Controls.Add(this.passLabel);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passText);
+            this.Controls.Add(this.userText);
+            this.Name = "MainForm";
+            this.Text = "SinZational Minecraft Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userText;
+        private System.Windows.Forms.TextBox passText;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label passLabel;
+    }
+}
+
